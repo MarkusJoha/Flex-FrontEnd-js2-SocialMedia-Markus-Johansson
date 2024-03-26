00b340a6c9be9690291e007136736174b027d78c
MarkusJoha <markus.johansson2@hotmail.com>
2024-03-26 21:57:47 +0100

    beginning of db conn

---
+++ src/scripts/database.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, child, get } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2d5Bt_tBKKa38-zHhdPzJntffPrYqCkA",
+  authDomain: "social-media-app-8f2ed.firebaseapp.com",
+  databaseURL: "https://social-media-app-8f2ed-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "social-media-app-8f2ed",
+  storageBucket: "social-media-app-8f2ed.appspot.com",
+  messagingSenderId: "281917889263",
+  appId: "1:281917889263:web:b3c64718ac2d4772b8991a"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export async function getDb() {
+  try {
+      const dbRef = ref(database);
+      const snapshot = await get(child(dbRef, `users/`));
+      if (snapshot.exists()) {
+          return snapshot.val();
+      } else {
+          throw new Error("No data available");
+      }
+  } catch (error) {
+      throw error;
+  }
+}
+
+
---
+++ src/scripts/registration.js
@@ -0,0 +1,31 @@
+import { getDb } from "./database";
+
+const email = document.getElementById('registration-email');
+const fullName = document.getElementById('registration-name');
+const password = document.getElementById('registration-password');
+const registerButton = document.getElementById('register-button');
+const images = document.querySelectorAll('input[name="profile-image"]');
+
+registerButton.onclick = async function (event) {
+
+    event.preventDefault();
+
+    let selectedImageSrc = '';
+    images.forEach(image => {
+        if (image.checked) {
+            selectedImageSrc = image.value;
+        }
+    });
+
+    console.log('Email: ', email.value);
+    console.log('Name: ', fullName.value);
+    console.log('Password: ', password.value);
+    console.log('Profile Image: ', selectedImageSrc);
+
+    try {
+        const db = await getDb(); 
+        console.log(db);
+    } catch (error) {
+        console.error(error);
+    }
+};
